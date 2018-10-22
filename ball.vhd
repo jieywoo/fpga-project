bef0a8c32008b906f4369087f486cb68e0356f8f
jyeonw <44306168+jyeonw@users.noreply.github.com>
2018-10-22 19:18:05 +0200

    Update ball.vhd

--- ball.vhd
+++ ball.vhd
@@ -1,6 +1,7 @@
 ----------------------------------------------------------------------------------
 -- Company: UPMC
 -- Engineer: Julien Denoulet
+--           Jieyeon Woo, Olivia Messina
 -- 
 --	Gestion de la Balle
 --
